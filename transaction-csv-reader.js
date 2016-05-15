72fd22ef647fcf665f29f51ea7ea5b70e280a7a3
Andrey Makhnutin <andrey.makhnutin@gmail.com>
2016-05-15 18:05:01 +0300

    adding lots of comments to csv reader

--- transaction-csv-reader.js
+++ transaction-csv-reader.js
@@ -4,20 +4,37 @@ var Stream = require('stream');
 var BabyParse = require('babyparse');
 var StringDecoder = require('string_decoder').StringDecoder;
 
+// This is kind of an arbitrary "minimum" set of transactions fields, without
+// which transactional file will provide little sense.
+// Mainly used to check wherther input stream is actually EN transactions file,
+// or just some nonsence.
 var EN_TRANSACTIONS_MANDATORY_FIELDS =
     ['Supporter Email', 'Campaign Type', 'Campaign ID'];
 
+// A subclass of Stream.Transform that transforms some input CSV stream into
+// JS objects representing EN Transactions.
+// The input data stream should emit Buffer objects with CSV utf8 encoded data.
+//
+// Main purpose of this class is to parse CSV data into objects and check for
+// CSV errors.
 class ENTransactionCSVReader extends Stream.Transform {
-    constructor(options) {
-        const streamOptions = {readableObjectMode: true};
-        if (options && options.highWaterMark !== undefined)
-            streamOptions.highWaterMark = options.highWaterMark;
-        super(streamOptions);
 
+    // options may be:
+    //     delimiter: ',' - CSV record delimiter value passed on to CSV parser.
+    //
+    constructor(options) {
+        super({readableObjectMode: true});
+
+        // if the pushed data chunk ends with an incomplete line, which happens
+        // most of the time, store the beginning of a line in a buffer.
         this._buffer = [];
 
+        // data chunks may end with an incomplete utf8 character, so we use
+        // StringDecored to help handle that.
         this._utf8Decoder = new StringDecoder('utf8');
 
+        // CSV parser config as passed on to Papa Parser.
+        // header is false because we extract header fields manually.
         this._csvConfig = {
             delimiter: ',',
             newline: '\n',
@@ -26,8 +43,10 @@ class ENTransactionCSVReader extends Stream.Transform {
         if (options && options.delimiter)
             this._csvConfig.delimiter = options.delimiter;
 
+        // column names of a currently downloaded CSV file.
         this._fieldNames = undefined;
 
+        // records downloaded so far.
         this.recordCount = 0;
     }
 
@@ -47,14 +66,17 @@ class ENTransactionCSVReader extends Stream.Transform {
                 missingFields);
     }
 
+    // parse and process a number of complete CSV lines.
     _processCSV(csv) {
         const self = this;
         const parsed = BabyParse.parse(csv, this._csvConfig);
 
+        // do not tolerate any kinds of errors in CSV.
         if (parsed.errors.length > 0) {
             throw new Error(`error parsing row ${self.recordCount + parsed.errors[0].row + 1}: ${parsed.errors[0].message}`);
         }
 
+        // extracting first row of the downloaded file - this will be header row.
         if (self._fieldNames === undefined) {
             self._fieldNames = parsed.data.shift();
 
@@ -63,29 +85,44 @@ class ENTransactionCSVReader extends Stream.Transform {
         const fieldNames = self._fieldNames, fieldsCount = fieldNames.length;
 
         for (let row of parsed.data) {
+            // putting each CSV row into an object with properties named after
+            // column names of a CSV file.
             let o = {};
 
+            // normally PapaParser would look out for incorrect numbers of columns,
+            // but in the extreme edge-cases, when all records in the chunk have
+            // different column number from all previous chunks, PapaParser will
+            // have no means to detect such atrifact.
             if (row.length != fieldsCount) {
                 throw new Error(`error parsing row ${self.recordCount + 1}: ${row.length} fields instead of ${fieldsCount}`);
             }
 
             for (let i = 0; i < fieldsCount; ++i) {
+                // usually the last column of a transactional CSV has empty name,
+                // which is silly, but that's the reality.
                 if (fieldNames[i] == '') continue;
                 o[fieldNames[i]] = row[i];
             }
 
+            // processing assembled object in a special way and pushing it down
+            // the readable stream.
             self.push(self._processTransaction(o));
+
             ++self.recordCount;
         }
     }
 
+    // here is where we receive raw chunks.
     _transform(chunk, encoding, callback) {
         const self = this;
         chunk = self._utf8Decoder.write(chunk);
 
         let csv;
         let i;
-        for (i = chunk.length; i >= 0; --i) {
+        // looking for last '\n' in the current chunk.
+        // buffered part from last chunk and everything before last '\n'
+        // will be parsed as CSV data.
+        for (i = chunk.length - 1; i >= 0; --i) {
             if (chunk[i] == '\n') {
                 self._buffer.push(chunk.substr(0, i));
                 csv = self._buffer.join('');
@@ -93,9 +130,14 @@ class ENTransactionCSVReader extends Stream.Transform {
                 break;
             }
         }
+        // everything after the last '\n' will be stored in the buffer
+        // to prepend to the next chunk.
         self._buffer.push(chunk.substr(i + 1));
 
+        // here, if i is negative, this means the chunk didn't have '\n' at all.
+        // so the whole chunk was an incomplete CSV line that we can't parse yet
         if (i >= 0) {
+            // _processCSV has many reasons to throw. Make sure to catch everything.
             try {
                 self._processCSV(csv);
             } catch(e) {
@@ -106,6 +148,12 @@ class ENTransactionCSVReader extends Stream.Transform {
         callback();
     }
 
+    // sometimes the file doesn't end with a '\n' character, usually when the file
+    // is incomplete. This means that the line will be still in the this._buffer
+    // because _transform parses only data that it found before '\n'.
+    // we need to treat it as a normal line and try to parse it to get the error
+    // it deserves. And who knows, maybe in the future even complete files will
+    // not have '\n' at the end.
     _flush(callback) {
         let csv = this._buffer.join('');
         this._buffer = [];
@@ -121,6 +169,9 @@ class ENTransactionCSVReader extends Stream.Transform {
         callback();
     }
 
+    // nothing fancy on the processing side, but plan to add a whole lot of classes
+    // specific to each transaction type, and distribute the transactions between them
+    // accordingly.
     _processTransaction(o) {
         return o;
     }
